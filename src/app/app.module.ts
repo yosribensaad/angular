6a6cd83b0dc40807b12f7e6975d78a968a0dbe0c
Yosri BEN SAAD <yosri.bensaad@esprit.tn>
2023-04-13 02:37:07 +0100

    Upload TODO2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
 import { ArticlesComponentComponent } from './articles-component/articles-component.component';
 import { FormsModule } from '@angular/forms';
+import { TemplateDrivenFormComponentComponent } from './template-driven-form-component/template-driven-form-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OffresEmploiComponentComponent,
-    ArticlesComponentComponent
+    ArticlesComponentComponent,
+    TemplateDrivenFormComponentComponent
   ],
   imports: [
     BrowserModule,
